626fd1de7b904e30d361c4ef228da8124fa889d1
zemkar <zemcharu@gmail.com>
2022-11-18 12:11:09 +0200

    data send

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import json
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
+data_var = [
+    {"name": "alfa", "age": 20}, 
+    {"name": "beta", "age": 21}, 
+    {"name": "gama", "age": 15},
+    ]
 
 
 @app.route("/")
@@ -13,6 +20,12 @@ def home():
 @app.route("/load")
 def loading():
     return  render_template ("loading_indicator.html")
+
+@app.route("/data")
+def data():
+    return  data_var
+
+    
 
 
 if __name__ == "__main__":
